45355232fe57232aca57dcd1b0f24ca50901b0e9
Victor Akerele <eneroakerele@gmail.com>
2021-04-08 11:27:55 +0100

    make options functional component

--- src/steps/OptionsStep.js
+++ src/steps/OptionsStep.js
@@ -1,38 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { TouchableOpacity, StyleSheet, Text, View, Dimensions } from 'react-native';
 import PropTypes from 'prop-types';
 
-class OptionsStep extends Component {
-  /* istanbul ignore next */
-  constructor(props) {
-    super(props);
-    this.onOptionClick = this.onOptionClick.bind(this);
-  }
+export default function OptionsStep(props) {
+  const styles = useStyle(props);
+  const { step } = props;
+  const { options } = step;
 
-  onOptionClick({ value }) {
-    this.props.triggerNextStep({ value });
-  }
-
-  render() {
-    const styles = useStyle(this.props);
-    const { step } = this.props;
-    const { options } = step;
-
-    return (
-      <View style={styles.body}>
-        {options.map((item, index) => (
-          <TouchableOpacity
-            key={item.value}
-            style={[styles.options, !index && styles.altOptions]}
-            onPress={() => this.onOptionClick({ value: item.value })}>
-            <View>
-              <Text style={[styles.text, !index && styles.altText]}>{item.label}</Text>
-            </View>
-          </TouchableOpacity>
-        ))}
-      </View>
-    );
-  }
+  return (
+    <View style={styles.body}>
+      {options.map((item, index) => (
+        <TouchableOpacity
+          key={item.value}
+          style={[styles.options, !index && styles.altOptions]}
+          onPress={() => props.triggerNextStep({ value: item.value })}>
+          <View>
+            <Text style={[styles.text, !index && styles.altText]}>{item.label}</Text>
+          </View>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
 }
 
 const useStyle = (props) =>
@@ -67,5 +55,3 @@ OptionsStep.propTypes = {
   optionStyle: PropTypes.object.isRequired,
   optionElementStyle: PropTypes.object.isRequired,
 };
-
-export default OptionsStep;
